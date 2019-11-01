a5a2fd24595a96ea377d48b779d243f5bd2ecdd7
blueag8 <ns.wickham08@outlook.com>
2019-11-01 20:13:19 +0000

    added user login functionality

--- auth_py/views.py
+++ auth_py/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
+from auth_py.forms import UserLoginForm
 # Create your views here.
 def index(request):
 
-    return render(request,"index.html")
+    return render(request,'index.html')
 
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "You have succesfully logged out")
+    return redirect(reverse('index'))
+
+def login(request):
+
+    if request.method=="POST":
+        login_form=UserLoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(username= request.POST['username'],
+                                     password= request.POST['password'])
+            messages.success(request, "You have successfully logged in ")
+            if user:
+               auth.login(user=user, request=request)
+            else:
+                login_form.add_error(None, "Your username or password is incorrect")
+
+    else:
+        login_form= UserLoginForm()
+    return render(request, 'login.html', {'login_form':login_form})
